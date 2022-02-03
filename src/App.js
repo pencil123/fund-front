b41abf0d828c94ad3ff8a0d91ac09d71587dd928
lyzhang <zhangluyan@haier.com>
2022-02-03 16:03:39 +0800

    change antd.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 import Index from "./page/Index";
+import "./antd.min.css";
 function App() {
   return (
     <BrowserRouter>
